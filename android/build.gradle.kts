0956360d753721386513f9f6b89accc14683d7f2
Jo Dev <gitlab.tadpole891@passfwd.com>
2026-07-18 23:54:11 +0000

    Edit build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,22 +1,13 @@
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
-}
-
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
